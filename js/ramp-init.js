14e3295ff35c1585eb2688646842946c78ab2d02
rbpollock <43445728+rbpollock@users.noreply.github.com>
2021-06-30 23:49:09 -0400

    stage 1 removing unifty branding

--- js/ramp-init.js
+++ js/ramp-init.js
@@ -1,34 +1,34 @@
 let currency = 'ETH';
 
-$(document).ready(function () {
-  addRampClickListener();
-  setCurrency();
+$(document).ready(function() {
+    addRampClickListener();
+    setCurrency();
 });
 
 const addRampClickListener = () => {
-  $("#ramp-btn").click(function(){
-      rampClickListener()
-  });
+    $("#ramp-btn").click(function() {
+        rampClickListener()
+    });
 };
 
 const rampClickListener = () => {
-  console.log("Selected currency: ", currency);
-  new rampInstantSdk.RampInstantSDK({
-    hostAppName: "Unifty",
-    hostLogoUrl: "https://unifty.io/assets/img/unifty2.png",
-    swapAsset: currency,
-    //url: 'https://ri-widget-staging.firebaseapp.com/', //remove this for the official release
-    hostApiKey: 'qohqhya4v3he5d7k3pdcbuahek3apweoemv7m92h',
-    variant: 'auto'
-  })
-    .on("*", (event) => console.log(event))
-    .show();
+    console.log("Selected currency: ", currency);
+    new rampInstantSdk.RampInstantSDK({
+            hostAppName: "Unifty",
+            hostLogoUrl: "https://127.0.0.1:8080/assets/img/wipp.png",
+            swapAsset: currency,
+            //url: 'https://ri-widget-staging.firebaseapp.com/', //remove this for the official release
+            hostApiKey: 'qohqhya4v3he5d7k3pdcbuahek3apweoemv7m92h',
+            variant: 'auto'
+        })
+        .on("*", (event) => console.log(event))
+        .show();
 };
 
 const setCurrency = () => {
     currency = getCurrency();
-    
-    if(currency != "xDai" && currency != "CELO" && currency != "MATIC" && currency != "ETH"){
-      currency = "ETH";
+
+    if (currency != "xDai" && currency != "CELO" && currency != "MATIC" && currency != "ETH") {
+        currency = "ETH";
     }
 }
